17abd58201f4e22efc65ea5f36e86d0c4ef43ac0
lancelot03001 <pktsangac@connect.ust.hk>
2020-05-11 22:00:16 +0800

    20200511 premeeting update2

--- SQLAlchemy.py
+++ SQLAlchemy.py
@@ -7,6 +7,91 @@ from sqlalchemy.dialects.mysql import \
         NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
         TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
 
+#Flask SQLAlchemy format
+class users(db.Model):
+    creationTime = db.Column(db.datetime, nullable = False, default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    login_name = db.Column(db.String(36), nullalbe = False)
+
+class business_users(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class individual_users(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class jobs(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class job_listings(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+
+class business_address(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class districts(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+
+class abnormality(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class job_type(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+
+class job_applications(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class enrollments(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class announcement(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class announcement_listings(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class verification(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class rating_category(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+
+class rating(db.Model):
+    id = db.Column(db.String(1000), primary_key = True)
+
+class review(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+class review_followup(db.Model):
+    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
+    id = db.Column(db.String(1000), primary_key = True)
+    columnName = db.Column(db.type, )
+
+#TEMPLATE
+# class name(db.Model):
+# columnName = db.Column(db.type, )
+
+
+#SQLAlchemy format
 metadata = MetaData()
 
 users = Table('users', metadata,
--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from sqlalchemy.dialects.mysql import \
-        BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
-        DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
-        LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, \
-        NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
-        TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
@@ -15,89 +11,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://public:b05qv-x4xca@localhost/ma
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+db.Model = automap_base(db.Model)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-class users(db.Model):
-    creationTime = db.Column(db.datetime, nullable = False, default = func.current_timestamp()
-    id = db.Column(db.String(1000), primary_key = True)
-    login_name = db.Column(db.String(36), nullalbe = False)
 
-class business_users(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class individual_users(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class jobs(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class job_listings(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-
-class business_address(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class districts(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-
-class abnormality(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class job_type(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-
-class job_applications(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class enrollments(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class announcement(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class announcement_listings(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class verification(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class rating_category(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-
-class rating(db.Model):
-    id = db.Column(db.String(1000), primary_key = True)
-
-class review(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-class review_followup(db.Model):
-    creationTime = db.Column(db.datetime(timezone = ture), nullable = False, server_default = func.current_timestamp())
-    id = db.Column(db.String(1000), primary_key = True)
-    columnName = db.Column(db.type, )
-
-#TEMPLATE
-# class name(db.Model):
-# columnName = db.Column(db.type, )
