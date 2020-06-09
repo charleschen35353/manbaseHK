81d185620330c588ba70fb06ec010de772366087
williswcy <williswcy@gmail.com>
2020-06-09 12:02:45 +0800

    Updated the UI for Login

--- manbase/forms.py
+++ manbase/forms.py
@@ -5,65 +5,70 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from manbase.models import User
 
+
 def password_requirement(form, field):
-# check if passwords contains 0-9 & a-z &A-Z and !@#$%
-   #TODO
-   if False:
-       raise ValidationError('Password must contain at least one number, one charater in upper case, one character in lower case, and a special character from \'@#!&*\'')
+    # check if passwords contains 0-9 & a-z &A-Z and !@#$%
+    # TODO
+    if False:
+        raise ValidationError(
+            'Password must contain at least one number, one charater in upper case, one character in lower case, and a special character from \'@#!&*\'')
+
 
 class RegistrationForm(FlaskForm):
 
-    username = StringField('Username', 
-                           validators=[DataRequired(), Length(min=5, max = 20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=20)])
     email = StringField('Email',
-                           validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-                           validators=[DataRequired(), Length(min=5, max=32)])
-    confirm_password = PasswordField('Comfirm Password', 
-                           validators= [DataRequired(), EqualTo('password', message="must match password")])
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=5, max=32)])
+    confirm_password = PasswordField('Comfirm Password',
+                                     validators=[DataRequired(), EqualTo('password', message="must match password")])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-             raise ValidationError('Username is taken. Please choose a different one')
+            raise ValidationError(
+                'Username is taken. Please choose a different one')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-             raise ValidationError('Email is taken. Please choose a different one')
-
+            raise ValidationError(
+                'Email is taken. Please choose a different one')
 
 
 class LoginForm(FlaskForm):
 
     email = StringField('Email',
-                           validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-                           validators=[DataRequired(), Length(min=5)])
-    remember = BooleanField('Rmember Me')
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=5)])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
 
 class UpdateAccountForm(FlaskForm):
 
-    username = StringField('Username', 
-                           validators=[DataRequired(), Length(min=5, max = 20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=20)])
     email = StringField('Email',
-                           validators=[DataRequired(), Email()])
-    picture = FileField('Updtae Profile Picture', validators = [FileAllowed(['jpg','png', 'jpeg'])])
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Updtae Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                 raise ValidationError('Username is taken. Please choose a different one')
+                raise ValidationError(
+                    'Username is taken. Please choose a different one')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                 raise ValidationError('Email is taken. Please choose a different one')
-
-
-     
+                raise ValidationError(
+                    'Email is taken. Please choose a different one')
