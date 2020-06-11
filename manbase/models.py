105ddf944407c18df37c18077567aa409fc2d50a
lancelot03001 <pktsangac@connect.ust.hk>
2020-06-11 22:19:17 +0800

    2020-06-11 completed database mapping

--- manbase/models.py
+++ manbase/models.py
@@ -1,11 +1,47 @@
 from manbase import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+Base = automap_base()
+
+# engine, suppose it has tables set up
+engine = create_engine("mysql://public:b05qv-x4xca@localhost/manbasedb")
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# mapped classes are now created with names by default
+# matching that of the table name.
+users = Base.classes.users
+business_users = Base.classes.business_users
+individual_users = Base.classes.individual_users
+jobs = Base.classes.jobs
+job_listings = Base.classes.job_listings
+business_address = Base.classes.business_address
+districts = Base.classes.districts
+abnormality = Base.classes.abnormality
+job_type = Base.classes.job_type
+job_applications  = Base.classes.job_applications
+enrollments = Base.classes.enrollments
+announcement = Base.classes.announcement
+announcement_listings = Base.classes.announcement_listings
+industry = Base.classes.industry
+verification = Base.classes.verification
+rating_category = Base.classes.rating_category
+rating = Base.classes.rating
+review = Base.classes.review
+review_followup = Base.classes.review_followup
+
+
+'''
+session = Session(engine)
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
@@ -15,16 +51,15 @@ class User(db.Model, UserMixin):
     profile_image = db.Column(db.String(20), nullable=False, default = "default.jpg")
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-        
-    '''
+
     __mapper_args__ = {
         'polymorphic_on':type
     }
-    '''
+
     def __repr__(self):
         return "User(\'{}\',\'{}\',\'{}\')".format(self.username, self.email, self.profile_image)
 
-'''
+
 class Individual_User(db.Model):
     __table_name__= "individual_users" 
     id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
@@ -33,7 +68,6 @@ class Individual_User(db.Model):
     # long list of attributes TODO
     def __repr__(self):
         return "User(\'{}\',\'{}\',\'{}\')".format(self.username, self.email, self.image_file)
-'''
 
 class Post(db.Model):
     __table_name__= "posts" 
@@ -45,3 +79,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return "Post(\'{}\',\'{}\')".format(self.title, self.date_posted)
+'''
