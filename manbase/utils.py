0a99e071527b8b10b4babb920011d6bc6df901df
charles <lc3533@columbia.edu>
2020-07-19 12:02:45 +0800

    verification sys done

--- manbase/utils.py
+++ manbase/utils.py
@@ -1,4 +1,5 @@
 from manbase import app, mail, db
+from manbase.models import *
 from flask_mail import Message
 from itsdangerous import URLSafeTimedSerializer
 import secrets
@@ -26,7 +27,7 @@ def generate_confirmation_token_for(user, purpose):
     db.session.commit()
     return serializer.dumps(user.ur_email + key, salt=app.config['SECURITY_PASSWORD_SALT'])
 
-def confirm_token_for(token, purpose ,expiration=3600): 
+def confirm_token_for(token, purpose ,expiration=18000): 
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     try:
         email_concatenated = serializer.loads(
@@ -34,19 +35,16 @@ def confirm_token_for(token, purpose ,expiration=3600):
             salt=app.config['SECURITY_PASSWORD_SALT'],
             max_age=expiration
         )
-        print(email_concatenated)
+
         key = email_concatenated[len(email_concatenated) - app.config['KEY_LENGTH']:len(email_concatenated)]
         email = email_concatenated[:len(email_concatenated) - app.config['KEY_LENGTH']]
-        user = User.query.filter_by(ur_email = email).first()
-        if purpose == "reset":
-            if user.ur_reset_key != key: return False
-        elif purpose == "email":
-            if user.ur_email_key != key: return False
-        elif purpose == "SMS":
-            if user.ur_SMS_key != key: return False
-        
-        return email  
+        user = Users.query.filter_by(ur_email = email).first()
+        if purpose == "reset" and user.ur_reset_key == key: return email
+        elif purpose == "email" and user.ur_email_key == key: return email
+        elif purpose == "SMS" and user.ur_SMS_key == key: return email 
+        else:
+            raise
     except:
-        return False
+        raise
 
     return email
